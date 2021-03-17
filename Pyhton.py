2c54623a5ad701bbea88fd308cd0fb00d9a55407
99003733 <78867415+99003733@users.noreply.github.com>
2021-03-17 11:52:48 +0530

    this is added

--- Pyhton.py
+++ Pyhton.py
@@ -1,31 +1,32 @@
 import pandas as pd
-import numpy as np
-
+from openpyxl import load_workbook
 
 sheet_1 = pd.read_excel('Book1.xlsx', sheet_name = 0)
 sheet_2 = pd.read_excel('Book1.xlsx', sheet_name = 1)
 sheet_3 = pd.read_excel('Book1.xlsx', sheet_name = 2)
 sheet_4 = pd.read_excel('Book1.xlsx', sheet_name = 3)
+sheet_5 = pd.read_excel('Book1.xlsx', sheet_name = 4)
 
 all_data_1 = pd.merge(sheet_1, sheet_2, how='left')
 all_data_2 = pd.merge(all_data_1, sheet_3, how='left')
 all_data_3 = pd.merge(all_data_2, sheet_4, how='left')
+all_data_4 = pd.merge(all_data_3, sheet_5, how='left')
 
+
+print(all_data_4)
 # df = pd.read_excel('Book1.xlsx', sheet_name = 'mastersheet')
-name = input("Enter the name : ")
+psnum = int(input("Enter the PS Number : "))
 
-listOfColumns = ["Display Name","PS number","Colour","Height","School","Sex","Age","Famsize","Mjob","Fjob",
+listOfColumns = ["PS number","Display Name","Colour","Height","School","Sex","Age","Famsize","Mjob","Fjob",
                  "Car","Room1","Reason","Guardian","Traveltime","Studytime","Failures","Schoolsup",
                  "Group","Date","Activities","Nursery","Higher","Internet","Romantic","Absences",
-                 "Class","Weight","SchoolName","Address","City","Postal","Phone"]
+                 "Class","Weight","SchoolName","Address","City","Postal","Phone","Principal","Community","Tstreet"
+                 ,"email","School Type","Country","Admission","Date","Year"]
 
-df1 = pd.DataFrame(all_data_3, columns = listOfColumns) 
-df1.set_index("Display Name",inplace = True)
+df1 = pd.DataFrame(all_data_4, columns = listOfColumns)
+df1.set_index("PS number",inplace = True)
 
-result = df1.loc[name]
-
-
-from openpyxl import load_workbook
+result = df1.loc[psnum]
 
 path = r"Book1.xlsx"
 book = load_workbook(path)
@@ -34,10 +35,11 @@ writer.book = book
 if 'mastersheet' in book.sheetnames:
     pfd = book['mastersheet'] 
     book.remove(pfd)
-    result.to_excel(writer, sheet_name = 'mastersheet')   
+result.to_excel(writer, sheet_name = 'mastersheet')
 
 writer.save()
 writer.close()
 
-result
+
+
 
