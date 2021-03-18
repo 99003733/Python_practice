5e95d92a7821a64d8e85ac611068cd6c039af510
Wasim M <66107425+m-wasim@users.noreply.github.com>
2021-03-18 18:19:24 +0530

    Update oo.py

--- folder experiment/oo.py
+++ folder experiment/oo.py
@@ -8,3 +8,15 @@ def search_data():
             if f_name.endswith('.xlsx'):
                 list_files.append(f_name)
         print(list_files)
+
+        
+def search_data():
+    path_file = {"D:\makefile":[] , "D:\Python Course": [], "D:\MININT" :[]}
+    #keys =  list(path.keys())
+    for path in path_file.keys():
+        print(path)
+        #list_files = []
+        for f_name in os.listdir(path):
+            if f_name.endswith('.xlsx'):
+                path_file[path].append(f_name)
+        #print(list_files)        
