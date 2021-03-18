a3634a9518ffdf5f6644b0d88d4786b7b6c185a7
99003733 <78867415+99003733@users.noreply.github.com>
2021-03-18 17:02:49 +0530

    Update multiple excel.py

--- folder experiment/multiple excel.py
+++ folder experiment/multiple excel.py
@@ -6,26 +6,47 @@ import os
 import pandas as pd
 from openpyxl import load_workbook
 
-def get_sheet():
+# def get_sheet():
+#     path = input()
+#     list_files = []
+#     for f_name in os.listdir(path):
+#         if f_name.endswith('.xlsx'):
+#             list_files.append(f_name)
+#     return list_files
+#     return path
+
+def search_data():
     path = input()
     list_files = []
     for f_name in os.listdir(path):
         if f_name.endswith('.xlsx'):
             list_files.append(f_name)
-    return list_files
-
-def search_data():
-    searched_data = get_sheet()
-    print(searched_data)
+    print(list_files)
     print('Wanna search more?????...')
     ans_more = input()
     while ans_more.lower() == 'yes':
-        more_se = get_sheet()
-        searched_data.extend(more_se)
+        more_se = search_data()
+        more_se.extend(list_files)
         print('Wanna search more?????...')
         ans_more = input()
         if ans_more.lower() == 'no':
             break
-    return searched_data
+    print(list_files)
+    for i in range(len(list_files)):
+        joiner = "".join(map(str,list_files[i]))
+        print(joiner)
+        conc = path+"\\"+joiner
+        print(conc)
+        workbook = load_workbook(filename=conc)
+        res = workbook.sheetnames
+        print("Total number of sheets in excel file are " + str(len(res)))
+        sheets = {}  # empty dictionary
+        for i in range(len(res)):
+            sheets["Sheet" + str(i)] = pd.read_excel(conc, engine="openpyxl", sheet_name=i)
+            sheets["Sheet" + str(i)].dropna(axis=1, how='all', inplace=True)
+            print(sheets["Sheet" + str(i)])
+
+
+
 search_data()
 
